e868e26a4d144daace16152c4eb53526de557eac
Caroline Fenlon <fenlonc@dub-mac-fenlon.eu.mrshmc.com>
2018-06-13 15:09:00 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server log');
+        }
+    });
+    next(); // needed to tell system it can move on to next middleware
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+});
+
+// order matters - doesn't work if before the app.set line above
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    // res.send( {
+    //     name: 'Caroline',
+    //     likes: [
+    //         'blah',
+    //         'other'
+    //     ]
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hello Website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'something went wrong'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
